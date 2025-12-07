24ddc5fa71196a0f31fd7a513a9edab75890e77b
RicardoMonteiroSimoes <30594398+RicardoMonteiroSimoes@users.noreply.github.com>
2025-12-07 21:34:09 +0100

    Add notNull to toString

--- backend/webapp-api/src/main/java/io/hephaistos/pyro/controller/dto/UserRegistrationRequest.java
+++ backend/webapp-api/src/main/java/io/hephaistos/pyro/controller/dto/UserRegistrationRequest.java
@@ -3,6 +3,7 @@ package io.hephaistos.pyro.controller.dto;
 import io.hephaistos.pyro.validation.ValidEmail;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
@@ -17,7 +18,7 @@ public record UserRegistrationRequest(@Schema(requiredMode = REQUIRED) @NotBlank
                                               message = "password can't be blank") String password) {
 
     @Override
-    public String toString() {
+    public @NotNull String toString() {
         return "UserRegistrationRequest[firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
     }
 }
