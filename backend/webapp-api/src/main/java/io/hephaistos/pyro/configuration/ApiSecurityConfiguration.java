eb8b0e81d8b705363c3de121a69841b4f456454c
RicardoMonteiroSimoes <30594398+RicardoMonteiroSimoes@users.noreply.github.com>
2025-12-09 20:21:59 +0100

    Allow CORS from localhost

--- backend/webapp-api/src/main/java/io/hephaistos/pyro/configuration/ApiSecurityConfiguration.java
+++ backend/webapp-api/src/main/java/io/hephaistos/pyro/configuration/ApiSecurityConfiguration.java
@@ -13,6 +13,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -47,6 +50,13 @@ public class ApiSecurityConfiguration {
                 .addFilterBefore(jwtOncePerRequestFilter,
                         UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 
 }
