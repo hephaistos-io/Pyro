7c2987b3d89c33e71c4ce07b96fcb305d9bcd71a
RicardoMonteiroSimoes <30594398+RicardoMonteiroSimoes@users.noreply.github.com>
2025-12-12 18:20:59 +0100

    Reset authentication in the frontend if 401 is returned.

--- webapp/src/app/interceptors/auth.interceptor.ts
+++ webapp/src/app/interceptors/auth.interceptor.ts
@@ -1,19 +1,30 @@
 import {HttpInterceptorFn} from '@angular/common/http';
 import {inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {catchError, throwError} from 'rxjs';
 import {AuthService} from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
   const token = authService.getToken();
 
+  let request = req;
   if (token) {
-    const clonedRequest = req.clone({
+    request = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(clonedRequest);
   }
 
-  return next(req);
+  return next(request).pipe(
+    catchError(error => {
+      if (error.status === 401) {
+        authService.logout();
+        router.navigate(['/login']);
+      }
+      return throwError(() => error);
+    })
+  );
 };
