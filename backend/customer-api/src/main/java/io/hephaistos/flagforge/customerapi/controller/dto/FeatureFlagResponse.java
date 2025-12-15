e6dfaa579f68a189dc1e8f42205173c7ec177d5a
RicardoMonteiroSimoes <30594398+RicardoMonteiroSimoes@users.noreply.github.com>
2025-12-15 23:32:49 +0100

    set customer-api to the minimum

--- backend/customer-api/src/main/java/io/hephaistos/flagforge/customerapi/controller/dto/FeatureFlagResponse.java
+++
@@ -1,4 +0,0 @@
-package io.hephaistos.flagforge.customerapi.controller.dto;
-
-public record FeatureFlagResponse(String key, Object value, String type) {
-}
--- backend/customer-api/src/main/java/io/hephaistos/flagforge/customerapi/service/ApiKeyInfo.java
+++
@@ -1,7 +0,0 @@
-package io.hephaistos.flagforge.customerapi.service;
-
-import java.util.UUID;
-
-public record ApiKeyInfo(UUID apiKeyId, UUID applicationId, UUID companyId,
-                         int rateLimitPerMinute) {
-}
